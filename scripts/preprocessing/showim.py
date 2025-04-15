9c292175b646909e22f72816996f48783028a85d
Omar Aguilar <omar.hawks8@gmail.com>
2025-04-15 11:48:59 -0400

    sens analysis

--- scripts/preprocessing/showim.py
+++ scripts/preprocessing/showim.py
@@ -2,16 +2,19 @@ import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 import matplotlib.image as mpimg
 
-impath = '/Users/omaraguilarjr/Downloads/0.png'
+impath = r"C:\Users\omarh\OneDrive - Georgia Institute of Technology\openEDS2019\openEDS\openEDS\S_0\0.png"
+impath2 = r"C:\Users\omarh\OneDrive - Georgia Institute of Technology\openEDS2019\openEDS\openEDS\S_0\1.png"
 img = mpimg.imread(impath)
+img2 = mpimg.imread(impath2)
+
+diff = img2 - img
 
 x_min, x_max, y_min, y_max = 159, 545, 166, 328
 width = x_max - x_min
 height = y_max - y_min
 
-fig, ax = plt.subplots()
-ax.imshow(img)
-rect = patches.Rectangle((x_min, y_min), width, height, linewidth=2, edgecolor='r', facecolor='none')
-ax.add_patch(rect)
+plt.figure()
+plt.set_cmap('gray')
+plt.imshow(diff)
 plt.axis('off')
 plt.show()
