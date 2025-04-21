f4131e0ebe0cba29e2e20656cf9b10b168b86e31
omaraguilarr84 <omar.hawks8@gmail.com>
2025-04-21 14:35:02 -0400

    new sens analysis

--- scripts/preprocessing/showim.py
+++ scripts/preprocessing/showim.py
@@ -1,13 +1,17 @@
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 import matplotlib.image as mpimg
+import numpy as np
 
-impath = r"C:\Users\omarh\OneDrive - Georgia Institute of Technology\openEDS2019\openEDS\openEDS\S_0\0.png"
-impath2 = r"C:\Users\omarh\OneDrive - Georgia Institute of Technology\openEDS2019\openEDS\openEDS\S_0\1.png"
+impath = r"/Users/omaraguilarjr/Library/CloudStorage/OneDrive-GeorgiaInstituteofTechnology/openEDS2019/openEDS/openEDS/S_0/0.png"
+impath2 = r"/Users/omaraguilarjr/Library/CloudStorage/OneDrive-GeorgiaInstituteofTechnology/openEDS2019/openEDS/openEDS/S_0/1.png"
 img = mpimg.imread(impath)
 img2 = mpimg.imread(impath2)
 
-diff = img2 - img
+diff = abs(img2 - img)
+
+threshold_value = 0.05
+binary_img = np.where(img > threshold_value, 1, 0).astype(np.float32)
 
 x_min, x_max, y_min, y_max = 159, 545, 166, 328
 width = x_max - x_min
@@ -15,6 +19,12 @@ height = y_max - y_min
 
 plt.figure()
 plt.set_cmap('gray')
-plt.imshow(diff)
+plt.imshow(binary_img)
 plt.axis('off')
 plt.show()
+
+# plt.hist(diff)
+# plt.title("Grayscale Histogram")
+# plt.xlabel("Pixel Intensity")
+# plt.ylabel("Frequency")
+# plt.show()
